ea43d636f9589f98762fcb350f00624e9aa31d9b
maguofu01 <maguofu01@zuoyebang.com>
2019-08-20 19:05:29 +0800

    [update]

--- project/index.php
+++ project/index.php
@@ -2,7 +2,6 @@
 
   
   $link = new mysqli('localhost', 'root', '123456', 'mydb');
-
   $userName = $_POST['nameValue'];
   $psd = $_POST['padValue'];
   if ($_POST['flag'] == 1) {
@@ -12,7 +11,8 @@
   // 插入sql
   $insertSql = 'insert into login (username, psd)
     values (' . '"' . $userName . '"' . ',' . $psd . ')';
-  // 查询sql
+  $checkBeforeInsert = 'select * from login where username=' . '"' . $userName . '"';
+  // 查询psd   sql
   $checkSql = 'select psd from login where username=' . '"' . $userName . '"';
   // 返回值格式
   $res = array(
@@ -25,18 +25,42 @@
 
   if (!$link->errno) {
     if(array_key_exists('confirmValue', $_POST)){
-      if($link->query($insertSql) === true){
-        // status=1插入成功
-        $res['data']['status'] = 1;
-      }else{
-        // status=2插入失败
+      // 注册
+      if ($link->query($checkBeforeInsert)->num_rows !== 0) {
+        // 用户名重复
         $res['data']['status'] = 2;
+        $res['data']['errMsg'] = '用户名已经被占用';
+      } else {
+        print_r($link->query($insertSql));
+        if($link->query($insertSql) === true){
+          // status=1插入成功
+          $res['data']['status'] = 1;
+          $res['data']['errMsg'] = '注册成功';
+        }else{
+          // status=2插入失败
+          $res['data']['status'] = 2;
+          $res['data']['errMsg'] = '注册失败，请重试';
+        }
       }
     }else{
+      // 登录
       // status=3登录成功
       // status=4登录失败
-      $checkResult = $link->query($checkSql);
-      print_r($checkResult);
+      if ($link->query($checkBeforeInsert)->num_rows === 0) {
+        $res['data']['status'] = 4;
+        $res['data']['errMsg'] = '没有该用户，请确认用户名是否存在';
+      } else {
+        $checkResult = $link->query($checkSql);
+        // print_r($checkResult->fetch_all()[0][0]);
+        $resultRow = $checkResult->fetch_assoc();
+        if ($resultRow['psd'] === $psd) {
+          $res['data']['status'] = 3;
+          $res['data']['errMsg'] = '登录成功';
+        } else {
+          $res['data']['status'] = 4;
+          $res['data']['errMsg'] = '密码错误，请重试';
+        }
+      }
     };
   } else {
     $res['errNo'] = 50000;
